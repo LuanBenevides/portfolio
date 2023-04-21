0bf2b2970b54e2a8d2dfaa2030e650483f286c6b
Luan Ferreira Benevides <Luan_benevides_ti@outlook.com>
2023-04-21 00:41:54 -0300

    conclusao da aba home e da aba projetos

--- src/components/Projetos/Projetos.js
+++ src/components/Projetos/Projetos.js
@@ -7,26 +7,34 @@ function Simple() {
         <div className='pattern-projects'>
             <h3>PROJETOS SIMPLES COM CONCEITOS BÁSICOS DE REACT.JS</h3>
             <article className='projects-board'>
-                <section className='pattern-card-tec'>
-                    <h3>JOGO DA VELHA</h3>
-                    <p>Jogue e se divirta, clicando <a href="#">aqui</a></p>
-                    <div className='image-block-pattern jv'></div>
-                </section>
-                <section className='pattern-card-tec'>
-                    <h3>BISCOITO DA SORTE</h3>
-                    <p>Veja o que a sua sorte diz <a href="#">hoje</a></p>
-                    <div className='image-block-pattern bds'></div>
-                </section>
-                <section className='pattern-card-tec'>
-                    <h3>CRONÔMETRO</h3>
-                    <p>Marque o tempo das suas <a href="#">atividades!</a></p>
-                    <div className='image-block-pattern cron'></div>
-                </section>
-                <section className='pattern-card-tec'>
-                    <h3>CALCULADORA</h3>
-                    <p>Faça contas na web, <a href="https://luanbenevides.github.io/react-calculator/" target='blank'>teste!</a></p>
-                    <div className='image-block-pattern calc'></div>
-                </section>
+                <a href='https://luanbenevides.github.io/jogo-da-velha/' target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>JOGO DA VELHA</h3>
+                        <p>Jogue e se divirta, clicando aqui</p>
+                        <div className='image-block-pattern jv'></div>
+                    </section>
+                </a>
+                <a href='https://luanbenevides.github.io/biscoito-da-sorte/' target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>BISCOITO DA SORTE</h3>
+                        <p>Veja o que a sua sorte diz hoje</p>
+                        <div className='image-block-pattern bds'></div>
+                    </section>
+                </a>
+                <a href='https://luanbenevides.github.io/cronometro/' target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>CRONÔMETRO</h3>
+                        <p>Marque o tempo das suas atividades!</p>
+                        <div className='image-block-pattern cron'></div>
+                    </section>
+                </a>
+                <a href="https://luanbenevides.github.io/react-calculator/" target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>CALCULADORA</h3>
+                        <p>Faça contas na web, teste!</p>
+                        <div className='image-block-pattern calc'></div>
+                    </section>
+                </a>
             </article>
         </div>
     );
@@ -37,39 +45,76 @@ function Advanced() {
         <div className='pattern-projects'>
             <h3>PROJETOS COMPLETOS, UTILIZANDO CONCEITOS AVANÇADOS DE REACT.JS</h3>
             <article className='projects-board'>
-                <section className='pattern-card-tec'>
-                    <h3>LISTA DE TAREFAS</h3>
-                    <p>Gerencie as suas atividades <a href="https://lb-app-tarefas.netlify.app/" target='blank'>aqui</a></p>
-                    <div className='image-block-pattern task'></div>
-                </section>
-                <section className='pattern-card-tec'>
-                    <h3>DEVFLIX</h3>
-                    <p>Filmes em cartaz em <a href="https://lb-devflix.netlify.app/" target='blank'>tempo real</a></p>
-                    <div className='image-block-pattern df'></div>
-                </section>
-                <section className='pattern-card-tec'>
-                    <h3>POKEDEX</h3>
-                    <p>Divirta-se com seu pokemon <a href="https://luanbenevides.github.io/pokedex/" target='blank'>favorito!</a></p>
-                    <div className='image-block-pattern pkdex'></div>
-                </section>
+                <a href="https://lb-app-tarefas.netlify.app/" target='blank'>
+                    <section className='pattern-card-tec' onClick={() => {}}>
+                        <h3>LISTA DE TAREFAS</h3>
+                        <p>Gerencie as suas atividades aqui</p>
+                        <div className='image-block-pattern task'></div>
+                    </section>
+                </a>
+                <a href="https://lb-devflix.netlify.app/" target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>DEVFLIX</h3>
+                        <p>Filmes em cartaz em tempo real</p>
+                        <div className='image-block-pattern df'></div>
+                    </section>
+                </a>
+                <a href='https://luanbenevides.github.io/pokedex/' target='blank'>
+                    <section className='pattern-card-tec'>
+                        <h3>POKEDEX</h3>
+                        <p>Divirta-se com seu pokemon favorito</p>
+                        <div className='image-block-pattern pkdex'></div>
+                    </section>
+                </a>
             </article>
         </div>
     );
 }
-export default function Projetos() {
-    const [simpleIsNext, setSimpleIsNext] = useState(true);
-   
-    function alternate() {
-        simpleIsNext ? setSimpleIsNext(false) : setSimpleIsNext(true);
-    }
-    
-    setInterval(() => {
-        alternate();
-    }, 8000);
 
-    if(simpleIsNext) {
-        return <Simple />
-    }else {
-        return <Advanced />
+function Paginator ({currentPage, handleNext, handleBack}) {
+    return(
+        <div className='arrows-board'>
+            <div className='back' currentPage={currentPage} onClick={handleBack}></div>
+            <div className='next' currentPage={currentPage} onClick={handleNext}></div>
+        </div>
+    );
+}
+export default function Projetos() {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    function handleNext () {
+        let newCurrentPage = currentPage;
+
+        if(currentPage >= 1) {
+            return;
+        }
+
+        newCurrentPage += 1;
+        setCurrentPage(newCurrentPage);
+    }
+
+    function handleBack() {
+        let newCurrentPage = currentPage;
+        if(currentPage === 0){
+            return;
+        }
+        newCurrentPage -= 1;
+        setCurrentPage(newCurrentPage);
+    }
+
+    if(currentPage === 0) {
+        return(
+            <>
+                <Simple />
+                <Paginator currentPage={currentPage} handleNext={handleNext} handleBack={handleBack}/>
+            </>
+        );
+    }else if(currentPage === 1){
+        return(
+            <>
+                <Advanced />
+                <Paginator currentPage={currentPage} handleNext={handleNext} handleBack={handleBack}/>
+            </>
+        );
     }
 }
